092ff2b6afdfdf5f0ace3434dd16fbc50c8d9061
Eperez77 <eperez77@uic.edu>
2020-02-22 17:14:50 -0600

    Added physics, playable character, basic scripting
    above

---
+++ Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Interaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("interactable"))
+        {
+            Debug.Log(other.name);
+            if (Input.GetButtonDown("fire1"))
+            {
+
+                jump = true;
+
+            }
+        }
+    }
+}
