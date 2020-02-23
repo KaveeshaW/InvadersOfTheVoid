d692aac9d34b28d2e5cb5417cc097b587d53de56
KaveeshaW <weerasirikaveesha@gmail.com>
2020-02-23 15:01:42 -0600

    all right hopefully this gives you access to all of the enemy stuff I have been working on

---
+++ Code/enemyVersionInvadersOfTheVoid/Assets/Scripts/enemyGraphics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+//changes the pixel art of the enemy to face the other way if they are using pathfinding
+public class enemyGraphics : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-4f, 4f, 4f);
+        } else if(aiPath.desiredVelocity.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(4f, 4f, 4f);
+        }
+    }
+}
