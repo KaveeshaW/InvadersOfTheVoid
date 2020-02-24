1ea4a6b0f9d784165421d52eb18692324c42a80d
Eperez77 <eperez77@uic.edu>
2020-02-24 10:44:05 -0600

    Basic scripting
    Basic scripting for lever

--- Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Interaction.cs
+++ Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Interaction.cs
@@ -7,14 +7,33 @@ public class Interaction : MonoBehaviour
 {
 
     private SpriteRenderer rend;
-    private Sprite catSprite, monsterSprite;
+    private Sprite leverSprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        leverSprite = Resources.Load<Sprite>("Medieval_props_free_12");
+        //colorRed = Resources.Load<Color>("FF0000");
+
+
+
+    } 
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("interactable"))
         {
+            //other.sprite = leverSprite;
+            //rend.sprite = leverSprite;
             Debug.Log(other.name);
-            if (Input.GetButtonDown("fire1"))
+            if (Input.GetButtonDown("Fire1"))
             {
+                rend.sprite = leverSprite;
+                //leverSprite = Resources.Load<leverSprite>("Medieval_props_free_12");
+                //rend.Sprite = new Sprite("Medieval_props_free_12");
 
             }
         }
