1e56af804e89fb1e2ad3a40ac9eff6bfb2b5aa42
Eperez77 <eperez77@uic.edu>
2020-02-25 02:43:51 -0600

    Added moving objects that activate when interacting with objects
    most of the changes were done in InteractLever

--- Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Scripts/InteractLever.cs
+++ Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Scripts/InteractLever.cs
@@ -7,11 +7,19 @@ public class InteractLever : MonoBehaviour
 {
 
     private SpriteRenderer rend;
+    SpriteRenderer chestRend;
     public Sprite leverSpritePulled;
     public Sprite leverSpriteUpright;
+    public Sprite chestClosed;
+    public Sprite chestOpen;
+    public GameObject movableLadder;
+    public GameObject chest;
+    public GameObject key;
+
 
     private bool leverFrontOf;
     //public Sprite mySprite = "Medieval_props_free_12";
+    //Vector3 pos = transform.position;
 
 
 
@@ -22,6 +30,8 @@ public class InteractLever : MonoBehaviour
         //leverSprite = Resources.Load("Medieval_props_free_12") as Sprite;
         leverFrontOf = false;
         //colorRed = Resources.Load<Color>("FF0000");
+        //pos.y = -4.58f;
+
 
 
 
@@ -31,12 +41,31 @@ public class InteractLever : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1") && leverFrontOf == true && rend.sprite == leverSpriteUpright)
         {
-            //rend.sprite = leverSprite;
-            Debug.Log("Hi mom from the lever");
+            //LEVER IS PULLED DOWN
+            //KEY SHOULD BE REVEALED
+            //LADDER TO DOOR SHOULD GO DOWN
+            Debug.Log("Hi from the lever");
 
-            //leverSprite = Resources.Load<leverSprite>("Medieval_props_free_12");
-            //rend.Sprite = new Sprite("Medieval_props_free_12");
+            //changes sprite of lever to that in the pulled down position
             rend.sprite = leverSpritePulled;
+            //adjusts scale and position of lever sprite so it looks consistent
+            Vector3 pos = new Vector3(1.346f, -4.56f, 0.0f);
+            Vector3 sca = new Vector3(1.342374f, 1.30707f, 1f);
+            transform.position = pos;
+            transform.localScale = sca;
+
+            //moves ladder down
+            movableLadder.transform.position = new Vector3(6.481f, -8.16f, 0.0f);
+
+            //exposes key if it isn't in inventory
+            key.transform.position = new Vector3(-4.4f, -4.65f, 0f);
+            chestRend = chest.GetComponent<SpriteRenderer>();
+            chestRend.sprite = chestOpen;
+
+
+
+
+
 
             //this.GetComponent<SpriteRenderer>().sprite = mySprite;
 
@@ -45,6 +74,23 @@ public class InteractLever : MonoBehaviour
         else if (Input.GetButtonDown("Fire1") && leverFrontOf == true && rend.sprite == leverSpritePulled)
         {
             rend.sprite = leverSpriteUpright;
+            Vector3 pos = new Vector3(1.5f, -4.46f, 0.0f);
+            Vector3 sca = new Vector3(1.2907f, 1.2907f, 1.2907f);
+
+
+            transform.position = pos;
+            transform.localScale = sca;
+
+            //moves ladder up
+            movableLadder.transform.position = new Vector3(6.481f, -5.49f, 0.0f);
+
+            //hides key in chest
+            key.transform.position = new Vector3(-4.4f, -6.65f, 0f);
+            chestRend = chest.GetComponent<SpriteRenderer>();
+            chestRend.sprite = chestClosed;
+
+            //6.481  -5.49
+
 
         }
     }
