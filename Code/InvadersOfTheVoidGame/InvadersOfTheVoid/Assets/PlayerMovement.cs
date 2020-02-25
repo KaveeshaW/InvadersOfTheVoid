eebf52b803949c6de9d54713c140b8f894a81f45
Adrian Zavala <adrianzavala42@gmail.com>
2020-02-24 18:55:36 -0600

    Animation and Wall bug
    Updated script and animator to make the player update when changing states. Added a physical 2d player so that it no longer sticks to walls.

--- Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/PlayerMovement.cs
+++ Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
-
+    public Animator animator;
+    
     public float runSpeed = 40f;
     bool jump = false;
     bool crouch = false;
@@ -18,14 +19,17 @@ public class PlayerMovement : MonoBehaviour
     {
         //Debug.Log(horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed);
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        //update the speed for animation movements
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
-
+            animator.SetBool("Jumping", true); //set the jumping to true for animation change
             jump = true;
 
         }
 
+        //not needed at the moment
         /*
         if (Input.GetButtonDown("Crouch"))
         {
@@ -35,10 +39,13 @@ public class PlayerMovement : MonoBehaviour
             crouch = false;
         }
         */
-
-
     }
 
+    public void PlayerLanding()
+    {
+        animator.SetBool("Jumping", false); //Change animation back
+    }
+    
     void FixedUpdate ()
     {
         //Move our character
