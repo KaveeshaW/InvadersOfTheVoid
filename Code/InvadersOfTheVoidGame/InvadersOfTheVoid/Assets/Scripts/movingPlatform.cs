d0755dde55cff8c869bd29de867217c229d7bd58
Kaveesha <weerasirikaveesha@gmail.com>
2020-03-10 21:54:06 -0500

    Revert "Kaveesha Test Push"
    This reverts commit ea5a17092e58f8da1954a2d058930f8e18ea7f01.

--- Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Scripts/movingPlatform.cs
+++ Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Scripts/movingPlatform.cs
@@ -8,7 +8,6 @@ public class movingPlatform : MonoBehaviour
     bool moveRight = true;
     
     void Update () {
-        //I want it to move more to the right because it doesn't go that far otherwise
         if(transform.position.x > 7.5f) {
             moveRight = false;
         }
@@ -20,19 +19,6 @@ public class movingPlatform : MonoBehaviour
         }
         else {
             transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
-        }
-    }
-
-    //when the player jumps on the moving platform, the player is now part of the platform
-    private void OnCollisionEnter2D(Collision2D col) {
-        if(col.transform.CompareTag("player")) {
-            col.collider.transform.SetParent(transform);
-        }
-    }
-    //when the player decides to move, they can
-    private void OnCollisionExit2D(Collision2D col) {
-        if(col.transform.CompareTag("player")) {
-            col.collider.transform.SetParent(null);
         }
     }
 }
