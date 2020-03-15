03041225fd615991ffa54513e39a790b45177334
Kaveesha <weerasirikaveesha@gmail.com>
2020-03-14 19:27:32 -0500

    health bar attempt
    the health bar almost works, it just cannot connect with player input because it cannot get a specific component from the health bar object

---
+++ Code/InvadersOfTheVoidGame/InvadersOfTheVoid/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    // Start is called before the first frame update
+    public void SetHealth(int health) {
+
+        slider.value = health;
+    }
+}
